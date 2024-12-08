ec09bec6f9f920829d80b0a32bd9823581eadc0a
luukleenders <luuk@digitalindividual.dev>
2024-12-08 10:39:28 +0100

    remove unused code

--- src/_unused/url.rs
+++
@@ -1,4 +0,0 @@
-pub const PLEX_HUBS_PROMOTED: &str = "/hubs/promoted";
-pub const PLEX_HUBS_SECTIONS: &str = "/hubs/sections";
-pub const PLEX_CONTINUE_WATCHING: &str = "/hubs/continueWatching";
-pub const PLEX_LIBRARY_METADATA: &str = "/library/metadata";
